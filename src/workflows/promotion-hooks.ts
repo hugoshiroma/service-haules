8c5ab445011d1cbc36b22bc4f9de2ee5639eb417
Hugo Gaio Shiroma <hugo.shiroma@gmail.com>
2025-12-11 14:15:19 -0300

    feat: Atualiza medusaJS

--- src/workflows/promotion-hooks.ts
+++ src/workflows/promotion-hooks.ts
@@ -1,31 +1,47 @@
-import { 
-  createPromotionsWorkflow,
-  deletePromotionsWorkflow
-} from '@medusajs/medusa/core-flows'
-import { generateCouponsWorkflow } from "./generate-coupons-workflow"
-import { removeCouponsWorkflow } from "./remove-coupons-workflow"
+import { createPromotionsWorkflow, updatePromotionsWorkflow } from '@medusajs/core-flows'
+import { deletePromotionsWorkflow } from '@medusajs/medusa/core-flows'
+import { generateCouponsWorkflow } from './generate-coupons-workflow'
+import { removeCouponsWorkflow } from './remove-coupons-workflow'
 
 // Hook para quando promoções são CRIADAS
-createPromotionsWorkflow.hooks.promotionsCreated(
-  async ({ promotions }, { container }) => {
-    for (const promotion of promotions) {
-      await generateCouponsWorkflow.run({ 
-        input: { promotionId: promotion.id },
-        container 
-      })
-    }
-  },
-)
+createPromotionsWorkflow.hooks.promotionsCreated(async ({ promotions }, { container }) => {
+  for (const promotion of promotions) {
+    await generateCouponsWorkflow.run({
+      input: { promotionId: promotion.id },
+      container,
+    })
+  }
+})
+
+// Hook para quando promoções são ATUALIZADAS
+// updatePromotionsWorkflow.hooks.promotionsUpdated(async ({ promotions }, { container }) => {
+//   const activePromotions = promotions.filter((p) => p.status === 'active')
+
+//   if (activePromotions.length === 0) {
+//     return
+//   }
+
+//   console.log(
+//     `[PromotionHooks] ${activePromotions.length} promoção(ões) tornaram-se ativas. Disparando workflows de geração de cupons.`,
+//   )
+
+//   for (const promotion of activePromotions) {
+//     await generateCouponsWorkflow.run({
+//       input: { promotionId: promotion.id },
+//       container,
+//     })
+//   }
+// })
 
 // Hook para quando promoções são DELETADAS
-deletePromotionsWorkflow.hooks.promotionsDeleted(
-  async ({ ids }, { container }) => {
-    for (const promotionId of ids) {
-      console.log(`[PromotionHooks] Promoção ${promotionId} foi deletada. Disparando workflow de remoção.`)
-      await removeCouponsWorkflow.run({
-        input: { promotionId: promotionId },
-        container
-      })
-    }
+deletePromotionsWorkflow.hooks.promotionsDeleted(async ({ ids }, { container }) => {
+  for (const promotionId of ids) {
+    console.log(
+      `[PromotionHooks] Promoção ${promotionId} foi deletada. Disparando workflow de remoção.`,
+    )
+    await removeCouponsWorkflow.run({
+      input: { promotionId: promotionId },
+      container,
+    })
   }
-)
+})
